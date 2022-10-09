e6a490d11d2d8b89de3362477801ec1333b03bd3
tracer-nju <tracer@nju.edu.cn>
2022-10-09 18:58:05 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 5654359fb69840beabceae8e6f94e815 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  18:58:05 up  2:07,  3 users,  load average: 0.93, 0.61, 0.41

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,11 +1,11 @@
 #include <stdint.h>
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  // uint64_t res = 0;
-  return 1;
-  // for(uint64_t i=0;i<a;i++){
-	// res += b;
-	// res %= m;	
-  // }
-  // return res;
+  uint64_t res = 0;
+  
+  for(uint64_t i=0;i<a;i++){
+	res += b;
+	res %= m;	
+  }
+  return res;
 }
