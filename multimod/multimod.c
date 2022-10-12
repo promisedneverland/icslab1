f9edf130fb98bc71958ae927ccac1ad0c495fa7e
tracer-nju <tracer@nju.edu.cn>
2022-10-12 20:43:39 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 548a3c8621d94b108fd253d1589213c0 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  20:43:39 up  2:06,  3 users,  load average: 0.39, 0.35, 0.23

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,11 +1,54 @@
 #include <stdint.h>
-
-uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+uint64_t m264;
+uint64_t mult(uint64_t mi, uint64_t b, uint64_t m)
+{
   uint64_t res = 0;
-  
-  for(uint64_t i=0;i<a;i++){
-	res += b;
-	res %= m;	
+  for(int i=mi;i >= 1;i--)
+  {
+    if(b & (1<<63))
+    {
+      add(res, mult(i-1,m264, m), m);
+
+    }
+    
+      b << 1;
+      b %= m;
+    
   }
+  return add(res, b, m);
+}
+uint64_t add(uint64_t res, uint64_t b, uint64_t m)
+{
+  a %= m;
+  b %= m;
+  if(res + b < b)
+  {
+    return b - m + res;
+  }
+  else 
+  {
+    return (res + b) % m;
+  }
+}
+uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+  uint64_t tmp = 0;
+  tmp = ~tmp;
+  tmp %= m;
+  tmp += 1;
+  m264 = tmp;
+  uint64_t res = 0;
+  uint64_t mask = 1;
+  a %= m;
+  b %= m;
+  for(int i=0;i<64;i++){
+    if(mask & a)
+      add(res, mult(i, b, m), m);
+
+    mask <<= 1;
+  }
+  // for(uint64_t i=0;i<a;i++){
+	// res += b;
+	// res %= m;	
+  // }
   return res;
 }
