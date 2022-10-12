93fa284eb211e32d6df57e2eeeb4652f44067486
tracer-nju <tracer@nju.edu.cn>
2022-10-12 21:36:11 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 548a3c8621d94b108fd253d1589213c0 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  21:36:11 up  2:58,  3 users,  load average: 0.48, 0.37, 0.39

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -14,8 +14,8 @@ uint64_t mod(uint64_t a,uint64_t b)
 }                                                           
 uint64_t add(uint64_t res, uint64_t b, uint64_t m)
 {
-  mod(res,m);
-  mod(b,m);
+  res = mod(res,m);
+  b = mod(b,m);
   if(res + b < b)
   {
     return b - m + res;
@@ -38,7 +38,7 @@ uint64_t mult(uint64_t mi, uint64_t b, uint64_t m)
     }
     
       b << 1;
-      mod(b,m);
+      b = mod(b,m);
     
   }
   return add(res, b, m);
@@ -47,14 +47,14 @@ uint64_t mult(uint64_t mi, uint64_t b, uint64_t m)
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t tmp = 0;
   tmp = ~tmp;
-  mod(tmp,m);
+  tmp = mod(tmp,m);
   tmp += 1;
   m264 = tmp;
 
   uint64_t res = 0;
   uint64_t mask = 1;
-  mod(a,m);
-  mod(b,m);
+  a = mod(a,m);
+  b = mod(b,m);
 
   for(int i=0;i<64;i++){
     if(mask & a)
