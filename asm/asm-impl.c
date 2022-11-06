45ee75028ba2b10fa93ad7114adf45ae9df1970b
tracer-nju <tracer@nju.edu.cn>
2022-11-06 13:12:24 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 514d173dd82c4b7d97f137fe877a6c25 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  13:12:24 up  1:06,  2 users,  load average: 0.24, 0.53, 0.52

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -16,7 +16,7 @@ int asm_popcnt(uint64_t x) {
   asm(
     ".L1:\n\t"
     "andq %[x],%[mask]\n\t"
-    "shr $10, %[x]\n\t"
+    "shrq $1, %[x]\n\t"
     "addq %[mask], %[res]\n\t"
     "movq $1,%[mask]\n\t"
     "cmpq $0,%[x]\n\t"
