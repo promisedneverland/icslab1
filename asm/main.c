ed4ee35def5bab50b0ef3b2ecb37ed477871b3f1
tracer-nju <tracer@nju.edu.cn>
2022-11-06 15:24:46 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 514d173dd82c4b7d97f137fe877a6c25 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  15:24:46 up  3:19,  2 users,  load average: 0.62, 0.53, 0.38

--- asm/main.c
+++ asm/main.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 int main() {
-  char s[5] = {1,1,0,0,0};
+  char s[5] = {'1',1,0,0,0};
   char d[5];
   asm_jmp_buf buf;
   int r = asm_setjmp(buf);
   if (r == 0) {
-    printf("%d\n",asm_add(1234, 5678) == 6912);
-    printf("%d\n",asm_popcnt(0x0123456789abcdefULL));
+    // printf("%d\n",asm_add(1234, 5678) == 6912);
+    // printf("%d\n",asm_popcnt(0x0123456789abcdefULL));
     // assert(asm_popcnt(0x000000000000000fULL) == 4);
     // TODO: add more tests here.
     // asm_memcpy(s,d,4);
