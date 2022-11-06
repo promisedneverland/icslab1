22f14757b3cc93b94b06e7e7cae32fe9d1940179
tracer-nju <tracer@nju.edu.cn>
2022-11-06 12:50:52 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 514d173dd82c4b7d97f137fe877a6c25 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  12:50:52 up 45 min,  2 users,  load average: 0.28, 0.43, 0.40

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -1,5 +1,5 @@
 #include "asm.h"
-#include <string.h>
+
 
 int64_t asm_add(int64_t a, int64_t b) {
   asm(
@@ -11,21 +11,34 @@ int64_t asm_add(int64_t a, int64_t b) {
 }
 
 int asm_popcnt(uint64_t x) {
-  int s = 0;
-  for (int i = 0; i < 64; i++) {
-    if ((x >> i) & 1) s++;
-  }
-  return s;
+  int res = 0;
+  uint64_t mask = 1;
+  asm(
+    ".L1:\n\t"
+    "andq %[x],%[mask]\n\t"
+    "addl %[mask], %[res]\n\t"
+    "movq $1,%[mask]\n\t"
+    "cmpq $0,%x"
+    "je .L1"
+
+    :[res] "+r" (res),[mask] "=r"(mask)
+    :[x] "r" (x)
+    :"cc"
+  ); 
+  return res; 
 }
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
-  return memcpy(dest, src, n);
+  // return memcpy(dest, src, n);
+  return NULL;
 }
 
 int asm_setjmp(asm_jmp_buf env) {
-  return setjmp(env);
+  // return setjmp(env);
+  return 0;
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
-  longjmp(env, val);
+  // longjmp(env, val);
+  return ;
 }
