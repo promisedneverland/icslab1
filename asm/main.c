f3f0ce29938a5479cd74af064f133bd6f16d04fe
tracer-nju <tracer@nju.edu.cn>
2022-11-06 13:27:24 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 514d173dd82c4b7d97f137fe877a6c25 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  13:27:24 up  1:21,  2 users,  load average: 0.86, 0.59, 0.50

--- asm/main.c
+++ asm/main.c
@@ -6,8 +6,8 @@ int main() {
   asm_jmp_buf buf;
   int r = asm_setjmp(buf);
   if (r == 0) {
-    printf("%d",asm_add(1234, 5678) == 6912);
-    printf("%d",asm_popcnt(0x0123456789abcdefULL));
+    printf("%d\n",asm_add(1234, 5678) == 6912);
+    printf("%d\n",asm_popcnt(0x0123456789abcdefULL));
     // assert(asm_popcnt(0x000000000000000fULL) == 4);
     // TODO: add more tests here.
     asm_longjmp(buf, 123);
