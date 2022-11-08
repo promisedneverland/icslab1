8c7096691da66ca4c224a31a4ca7bd1af9e9c5e3
tracer-nju <tracer@nju.edu.cn>
2022-11-08 17:07:23 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 7e44efb7306e4607a24cdf9c897a13d7 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  17:07:23 up 33 min,  3 users,  load average: 0.38, 0.34, 0.37

--- asm/main.c
+++ asm/main.c
@@ -6,6 +6,7 @@ int main() {
   // char s[5] = {'1','5','0','4','5'};
   // char d[5] = {0,'0',0,'4','5'};
   asm_jmp_buf buf = {};
+  buf[2] = 0;
   int r = asm_setjmp(buf);
   if (r == 0) {
     // printf("%d\n",asm_add(1234, 5678) == 6912);
