dabc8c51ebea1f41ea9194be2ebc4e1261404602
tracer-nju <tracer@nju.edu.cn>
2022-11-08 18:44:40 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: 7e44efb7306e4607a24cdf9c897a13d7 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  18:44:40 up  2:10,  4 users,  load average: 0.34, 0.34, 0.34

--- asm/asm.h
+++ asm/asm.h
@@ -2,9 +2,9 @@
 #include <stddef.h>
 
 // TODO: replace with your definition
-#include <setjmp.h> // remove this
-#define asm_jmp_buf jmp_buf
-// typedef char asm_jmp_buf[28];
+// #include <setjmp.h> // remove this
+// #define asm_jmp_buf jmp_buf
+typedef char asm_jmp_buf[28];
 int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
 void   *asm_memcpy(void *dest, const void *src, size_t n);
