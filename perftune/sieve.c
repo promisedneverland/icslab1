ed8e769c5552edf9c57a1a3fe9a1b8776ee91b07
tracer-nju <tracer@nju.edu.cn>
2022-11-29 15:38:17 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: adb572666d214e0c8e8a4933212be566 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  15:38:17 up 52 min,  2 users,  load average: 0.62, 0.58, 0.55

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -2,20 +2,26 @@
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
-#include <math.h>
 #define N 10000000
 #define blocksize 100
 static bool is_prime[N];
 static int  primes[N];
 
+int sqrt(int n)
+{
+  int i = 1;
+  while(i*i < n)
+    i++;
+  return i - 1;
+}
 int *sieve(int n) {
   assert(n + 1 < N);
   for (int i = 0; i <= n; i++)
     is_prime[i] = true;
 
   int blocknum = n/blocksize + 1;
-  float dn = n;
-  int nsqrt = sqrt(dn);
+ 
+  int nsqrt = sqrt(n);
 
   for (int i = 2; i <= n; i++) {
     if(is_prime[i])
