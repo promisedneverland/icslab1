4069501d9d7dff014e0951f57a3c8798dc66c28c
tracer-nju <tracer@nju.edu.cn>
2022-11-29 15:31:58 +0800

    Static hostname: linux        Icon name: computer-desktop          Chassis: desktop       Machine ID: 552f2465cfa54fb0b6e88d3cd9199c7b          Boot ID: adb572666d214e0c8e8a4933212be566 Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-47-generic     Architecture: x86-64  Hardware Vendor: Micro-Star International Co., Ltd.   Hardware Model: MS-7D42  15:31:58 up 46 min,  2 users,  load average: 0.54, 0.73, 0.58

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -13,6 +13,10 @@ int *sieve(int n) {
   for (int i = 0; i <= n; i++)
     is_prime[i] = true;
 
+  const int blocksize = 100;
+  const int blocknum = N/blocksize;
+  int nsqrt = sqrt(n);
+  
   for (int i = 2; i <= n; i++) {
     if(is_prime[i])
     for (int j = i + i; j <= n; j += i) {
